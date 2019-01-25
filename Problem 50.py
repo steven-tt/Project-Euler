642535f3c47b4f09f9632de5c0fa40208e00f7fc
Steven Turner <tstupidt@users.noreply.github.com>
2019-01-25 11:42:13 -0800

    Changes to problem 50

--- Problem 50.py
+++ Problem 50.py
@@ -10,28 +10,39 @@ def is_prime(num):
     return True
 
 def consect_prime(lim):
-
-    max_len = 0
-    num = 0
-    for i in range(lim):
+    list_of_primes = [i for i in range(lim) if is_prime(i)]
+    for i in range(lim,0,-1):
         if is_prime(i):
-            #print(i)
-            sum = 0
-            sum_len = 0
-            for j in range(i,lim):
-                if is_prime(j):
-                    # print(j)
-                    sum += j
-                    sum_len += 1
-                    if sum >= lim:
-                        break
-                    elif is_prime(sum) and sum_len > max_len:
-                        #print(sum,sum_len)
-                        num = sum
-                        max_len = sum_len
+            for j in range()
 
-    return num
+consect_prime(100)
 
-start = time.time()
-consect_prime(1000000)
-print(time.time()-start)
+
+
+
+
+        # max_len = 0
+        # num = 0
+        # for i in range(lim):
+        #     if is_prime(i):
+        #         #print(i)
+        #         sum = 0
+        #         sum_len = 0
+        #         for j in range(i,lim):
+        #             if is_prime(j):
+        #                 # print(j)
+        #                 sum += j
+        #                 sum_len += 1
+        #                 if sum >= lim:
+        #                     break
+        #                 elif is_prime(sum) and sum_len > max_len:
+        #                     #print(sum,sum_len)
+        #                     num = sum
+        #                     max_len = sum_len
+        #
+        # return num
+
+
+# start = time.time()
+# consect_prime(1000000)
+# print(time.time()-start)
